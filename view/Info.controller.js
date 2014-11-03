4ea230e58e9d6d9e85c09154ee672249077fbc44
Fabio Pagoti <fabiopagoti@gmail.com>
2014-11-03 14:42:45 -0200

    Template created using UI5 Boilerplate

---
+++ view/Info.controller.js
@@ -0,0 +1,10 @@
+sap.ui.controller("ui5bp.view.Info", {
+
+    onInit: function() {
+        this.bus = sap.ui.getCore().getEventBus();
+    },
+
+    doNavBack: function(event) {
+        this.bus.publish("nav", "back");
+    } 
+});
